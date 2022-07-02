0eb6283de9d37ff8bba9a3dfdc44da63419956a9
piwiit <bouillaux@hotmail.fr>
2022-07-02 13:57:28 +0200

    add eventListerner form

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
-let search = document.querySelector('input');
+let input = document.querySelector('input');
+let form = document.querySelector('form');
 let mealData = [];
 
-document.addEventListener('submit', (e) => {
-  e.preventDefault();
-  mealsDisplay();
-});
-
-const fetchMeals = async () => {
-  await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + search.value)
+const fetchMeals = async (search) => {
+  await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + search)
     .then((res) => res.json())
     .then((data) => (mealData = data.meals));
+  console.log(mealData);
 };
-const mealsDisplay = async () => {
-  await fetchMeals();
 
-  console.log(mealData[0]);
+const mealsDisplay = () => {
+  mealData.length = 12;
 
   result.innerHTML = mealData
     .map(
-      (meal) => `
-        <li>
-          <p>${meal.strMeal}</p>
+      (meal) =>
+        `
+    <li>
+          <h2>${meal.strMeal}</h2>
+          <p>${meal.strArea}</p>          
           <img src='${meal.strMealThumb}'>
-        </li>
-    `
+          </li>
+          `
     )
     .join(' ');
 };
+
+input.addEventListener('input', (e) => {
+  fetchMeals(e.target.value);
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  mealsDisplay();
+});
